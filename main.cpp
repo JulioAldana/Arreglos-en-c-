a044e1c13986ca8d4c0cc46e13ce152c01449200
JulioAldana <jaldanav2@miumg.edu.gt>
2025-04-05 10:30:52 -0600

    estructura del menu

--- main.cpp
+++ main.cpp
@@ -8,6 +8,18 @@ int main() {
     int cantidad = 0;   // la cantidad de tareas registradas
     int opcion;
 
+    do {
+        cout << "\nAGENDA DE TAREAS\n";
+        cout << "1. Agregar tarea\n";
+        cout << "2. Ver tareas\n";
+        cout << "3. Guardar tareas en archivo\n";
+        cout << "4. Salir\n";
+        cout << "Elige una opción: ";
+        cin >> opcion;
+        cin.ignore();
+
+
+    } while (opcion != 4);
 
     return 0;
 }
